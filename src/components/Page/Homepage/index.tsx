b32152ee152692a1404f7cfe66a7f6ef7f4b13a1
Henrique Lopes <h.lopes.silva2015@gmail.com>
2023-05-28 22:35:39 -0300

    Update

--- src/components/Page/Homepage/index.tsx
+++ src/components/Page/Homepage/index.tsx
@@ -6,11 +6,23 @@ export function Homepage() {
       <section className={styles.main}>
         <div className="mx-auto max-w-7xl w-full">
           <div className={styles.presentation}>
-            <p className={styles.presentationText}>
+            <p className={styles.presentationTitle}>
               Uma refeição doada, um coração aquecido
+            </p>
+            <p className={styles.presentationSubtitle}>
+              Seja bem-vindo ao nosso site dedicado à doação de comida e
+              plantação de alimentos!
+            </p>
+            <p className={styles.presentationSubtitle}>
+              Aqui, nosso objetivo é promover a solidariedade e a
+              sustentabilidade alimentar.
             </p>
           </div>
         </div>
+      </section>
+
+      <section className="bg-emerald-500 h-52">
+        <div className="mx-auto max-w-7xl w-full"></div>
       </section>
     </main>
   );
