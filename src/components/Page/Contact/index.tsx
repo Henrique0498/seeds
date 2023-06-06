76a5d9eb20bf5a1dfb1ab11f93c1e10fc2b9fd4c
Caio Ribeiro Rodrigues <caiorrodrigues2004@gmail.com>
2023-06-05 23:53:30 -0300

    ok

--- src/components/Page/Contact/index.tsx
+++ src/components/Page/Contact/index.tsx
@@ -1,5 +1,86 @@
 import styles from "./Contact.module.css";
 
 export function Contact() {
-  return <main className={styles.container}>Contatos</main>;
-}
+  return (
+    <main className={styles.container}>
+      <section>
+        <div className="mx-auto max-w-7xl w-full p-4 py-8">
+          <form>
+            <div className="space-y-12">
+              <div className="border-b border-gray-900/10 pb-12">
+                <h2 className="text-base font-semibold leading-7 text-gray-900">
+                  Informações pessoais
+                </h2>
+                <p className="mt-1 text-sm leading-6 text-gray-600">
+                  Preencha os campos abaixo para entrar em contato com a gente.
+                </p>
+
+                <div className="mt-10 grid grid-cols-1 gap-x-4 gap-y-4 sm:grid-cols-6">
+                  <div className="sm:col-span-3">
+                    <label
+                      htmlFor="first-name"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Primeiro nome
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        type="text"
+                        name="first-name"
+                        id="first-name"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 px-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+                <div className="sm:col-span-3">
+                    <label
+                      htmlFor="last-name"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Sobrenome
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        type="text"
+                        name="last-name"
+                        id="last-name"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 px-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+                  <div className="sm:col-span-2">
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Email
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 px-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="sm:col-span-2">
+                    <label
+                      htmlFor="gender"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Sexo
+                    </label>
+                    <div className="mt-2">
+                      <select
+                        id="gender"
+                        name="gender"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 px-3 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600  sm:text-sm sm:leading-6"
+                      >
+                        <option>Nenhum seleciona</option>
+                        <option>Masculino</option>
+                        <option>Feminino</option>
+                      </select>
+                    </div>
+                  </div>
