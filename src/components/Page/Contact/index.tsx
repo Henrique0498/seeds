a916dbb62f3afdcf681dcb01d3bbbba794bcced8
Caio Ribeiro Rodrigues <caiorrodrigues2004@gmail.com>
2023-06-05 23:54:16 -0300

    uptade

--- src/components/Page/Contact/index.tsx
+++ src/components/Page/Contact/index.tsx
@@ -84,3 +84,41 @@ export function Contact() {
                       </select>
                     </div>
                   </div>
+                  <div className="sm:col-span-2">
+                    <label
+                      htmlFor="gender"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Tipo de pessoa
+                    </label>
+                    <div className="mt-2">
+                      <select
+                        id="gender"
+                        name="gender"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 px-3 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      >
+                        <option>Nenhum seleciona</option>
+                        <option>Física</option>
+                        <option>Jurídica</option>
+                      </select>
+                    </div>
+                  </div>
+
+                  <div className="col-span-full">
+                    <label
+                      htmlFor="address"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Endereço completo
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        type="text"
+                        name="address"
+                        id="address"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 px-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+
+                  
