fc99d453c367f68e54b3a368b8048751639fdf76
Caio Ribeiro Rodrigues <caiorrodrigues2004@gmail.com>
2023-06-05 23:55:24 -0300

    uptade

--- src/components/Page/Contact/index.tsx
+++ src/components/Page/Contact/index.tsx
@@ -154,3 +154,22 @@ export function Contact() {
                       />
                     </div>
                   </div>
+
+                  <div className="sm:col-span-2">
+                    <label
+                      htmlFor="cep"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      CEP
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        type="text"
+                        name="cep"
+                        id="cep"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 px-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
