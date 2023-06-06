79dd26239e0ab40a6757aeb35a8b2725b32f2008
Caio Ribeiro Rodrigues <caiorrodrigues2004@gmail.com>
2023-06-05 23:54:47 -0300

    uptade

--- src/components/Page/Contact/index.tsx
+++ src/components/Page/Contact/index.tsx
@@ -137,3 +137,20 @@ export function Contact() {
                       />
                     </div>
                   </div>
+
+                  <div className="sm:col-span-2">
+                    <label
+                      htmlFor="region"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Estado
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        type="text"
+                        name="region"
+                        id="region"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 px-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
