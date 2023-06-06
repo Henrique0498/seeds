fbccf90b96672703bcd80c3f65c0f69ed277b610
Caio Ribeiro Rodrigues <caiorrodrigues2004@gmail.com>
2023-06-05 23:56:12 -0300

    uptade

--- src/components/Page/Contact/index.tsx
+++ src/components/Page/Contact/index.tsx
@@ -173,3 +173,25 @@ export function Contact() {
                   </div>
                 </div>
               </div>
+
+              <div className="border-b border-gray-900/10 pb-12">
+                <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                  <div className="col-span-full">
+                    <label
+                      htmlFor="about"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Motivo de contato
+                    </label>
+                    <div className="mt-2">
+                      <textarea
+                        id="about"
+                        name="about"
+                        rows={3}
+                        className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
